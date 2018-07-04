cfe08397bd2336dc06e61cef7af0c630d0d7d1e4
kkrawczynski <kkrawczynski@it-dev.pl>
2018-07-04 13:37:48 +0200

    Working on big template

--- Script/main.js
+++ Script/main.js
@@ -1,10 +1,23 @@
-var data, template, html, id;
+var data, template, template2, templateBig, html, id, allFields;
 var url = "http://jsonplaceholder.typicode.com/posts";
 
 
 $(document).ready(function(){
     $.getJSON(url, function(randomObj) {
         
+        templateBig = `<div class="row">
+        <div class="col-3">
+              <ul id="userId">Users list</ul>
+        </div>
+        <div class="col-9">
+              <h1 id="templateArea">Test</h1>
+        </div>
+        </div>`;
+
+        allFields = Mustache.to_html(templateBig,"something");
+            $('#myTemplate').append(allFields);
+        
+    
         // Pobieranie wszystkich nagłówków
         template ="<h1>{{title}}</h1>";
         randomObj.forEach(title => {
@@ -18,7 +31,7 @@ $(document).ready(function(){
             id = Mustache.to_html(template2,id);
             $('#userId').append(id);
         });
-
+       
 
     });
 });
